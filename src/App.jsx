e146910c405d4cb3dd19126e9560fbdd715f7abd
quyenpham00 <85977597+quyenpham00@users.noreply.github.com>
2025-03-09 15:19:00 +0700

    Revert "basic layout"

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,87 @@
 import './App.css'
-import Board from './pages/Boards/_id'
+import Button from '@mui/material/Button'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import Box from '@mui/material/Box'
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+  const handleChange = (event) => {
+    setMode(event.target.value)
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="demo-select-small-label">Mode</InputLabel>
+      <Select
+        labelId="demo-select-small-label"
+        id="demo-select-small"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <Box
+            display="flex"
+            alignItems="center"
+            gap={1}
+          >
+            <LightModeIcon/> Light
+          </Box>
+        </MenuItem>
+        <MenuItem value="dark">
+          <Box
+            display="flex"
+            alignItems="center"
+            gap={1}
+          >
+            <DarkModeOutlinedIcon/> Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value="system">
+          <Box
+            display="flex"
+            alignItems="center"
+            gap={1}
+          >
+            <SettingsBrightnessIcon/> System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
 
 function App() {
 
   return (
     <>
-      <Board />
+      <ModeSelect/>
+      <ModeToggle/>
+      <div>test</div>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
     </>
   )
 }
--- src/theme.js
+++ src/theme.js
@@ -2,10 +2,6 @@ import { cyan, deepOrange, orange, teal } from '@mui/material/colors'
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 
 const theme = extendTheme({
-  trello:{
-    appBarHeight: '58px',
-    boardBarHeight: '60px'
-  },
   colorSchemes: {
     light: {
       palette: {
